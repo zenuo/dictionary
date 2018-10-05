1a733defce045078606caaba7292dacf18feeb37
zenuo <zenuo@protonmail.com>
2018-10-05 13:43:47 +0800

    Create script

--- README.md
+++ README.md
@@ -1,16 +1,40 @@
-End-of-Life Notice
+# Google Dictionary python script
 ------------------
 
-The Google's Unofficial Dictionary API is deprecated.
-
-This project is no longer maintained. Please use repository history to access previous content.
-
-Thank you for your interest.
-
----
-
-Google Dictionary Chrome Extension uses another API, but there is a referer restriction configured on the API key.
-
+## Basic
 ``` sh
 curl 'https://content.googleapis.com/dictionaryextension/v1/knowledge/search?term=query&language=en&key=AIzaSyC9PDwo2wgENKuI8DSFOfqFqKP2cKAxxso' -H 'x-origin: chrome-extension://mgijmajocgfcbeboacabfgobmjgjcoja'
 ```
+
+## Requirements
+Because of (Great Firewall)[https://en.wikipedia.org/wiki/Great_Firewall], we have to request Google API over a proxy. 
+
+This script use (socks5)[https://en.wikipedia.org/wiki/SOCKS#SOCKS5] as the proxy, Install (PySocks)[https://github.com/Anorov/PySocks] :
+
+```sh
+pip3 install -U pysocks
+```
+
+and, edit script to your proxy setting:
+
+```python
+# change your proxy's ip
+ip = '127.0.0.1'
+# change your proxy's port
+port = 1086
+```
+
+## Using
+> You can add this script to your `PATH`
+```sh
+$ ./google-dictionary.py dictionary
+dictionary |ˈdɪkʃ(ə)n(ə)ri|
+
+[noun]
+   a book or electronic resource that lists the words of a language (typically in alphabetical order) and gives their meaning, or gives the equivalent words in a different language, often also providing information about pronunciation, origin, and usage.
+     • I'll look up 'love' in the dictionary
+     • the website gives access to an online dictionary
+     • the dictionary definition of ‘smile’
+```
+
+Thanks for your intrest :)
---
+++ google-dictionary.py
@@ -0,0 +1,70 @@
+#!/usr/local/python3
+
+import socket
+import requests
+import socks
+from sys import argv
+import urllib
+import json
+
+# get string of sense family
+def senseFamilyStr(entry: dict):
+    result = ''
+    for index, senseFamily in enumerate(entry['senseFamilies'], start=1):
+        partsOfSpeechs = senseFamily['partsOfSpeechs'][0]['value']
+        sensesStr = f'[{partsOfSpeechs}]\r\n'
+        for index, sense in enumerate(senseFamily['senses'], start=1):
+            definition = sense['definition']['text']
+            senseStr = f'   {definition}\r\n'
+            if 'exampleGroups' in sense.keys():
+                for exampleGroup in sense['exampleGroups']:
+                    example = exampleGroup['examples'][0]
+                    senseStr += f'     • {example}\r\n'
+            sensesStr += senseStr
+        result += sensesStr
+    return result
+
+
+# change your proxy's ip
+ip = '127.0.0.1'
+# change your proxy's port
+port = 1086
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, ip, port)
+socket.socket = socks.socksocket
+
+
+# url
+PATH = 'https://content.googleapis.com/dictionaryextension/v1/knowledge/search'
+term = ' '.join(argv[1:])
+query = {
+    'language': 'en',
+    'key': 'AIzaSyC9PDwo2wgENKuI8DSFOfqFqKP2cKAxxso',
+    'term': term
+}
+url = PATH + '?' + urllib.parse.urlencode(query)
+# headers
+headers = {
+    'x-origin': 'chrome-extension://mgijmajocgfcbeboacabfgobmjgjcoja'
+}
+# make the request, and decode the response to dict
+response = json.loads(requests.get(url, headers=headers).text)
+
+#if response not has key 'dictionaryData', exit
+if 'dictionaryData' not in response.keys():
+    print(f'Sorry, entry [{term}] not found.')
+    exit(-1)
+
+# get entry
+entry = response['dictionaryData'][0]['entries'][0]
+
+# construct output string
+headword = entry['headword']
+phonetics = ''
+if 'phonetics' in entry.keys():
+    phonetics = '|' + entry['phonetics'][0]['text'] + '|'
+
+senseFamily = senseFamilyStr(entry)
+
+# output
+print(f'{headword} {phonetics}\r\n\r\n{senseFamily}')
+exit(0)
